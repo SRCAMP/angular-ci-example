8530b9573c9c87743a03e1dbf4dd288de0ce50c3
Satheesh Kumar Routhu <satheesh.k.routhu-1@lowes.com>
2018-05-04 11:40:45 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RedComponent } from './red/red.component';
+import { BlueComponent } from './blue/blue.component';
+import { AppRoutingModule } from './AppRoutingModule';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedComponent,
+    BlueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
