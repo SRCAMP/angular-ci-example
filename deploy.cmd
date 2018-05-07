af28f3a82d396afa25fdabb79a39ba42071218a0
Satheesh Kumar Routhu <satheesh.k.routhu-1@lowes.com>
2018-05-06 22:06:30 -0400

    testing build command.

--- deploy.cmd
+++ deploy.cmd
@@ -106,6 +106,7 @@ pushd "%DEPLOYMENT_SOURCE%"
 :: call :ExecuteCmd !NPM_CMD! run build
 :: If the above command fails comment above and uncomment below one
   call ./node_modules/.bin/ng build –prod
+:: testing above command.
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
